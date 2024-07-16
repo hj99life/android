975ca9eb8e76c4fe3fe29ae349ce13dcb45a4237
hj99life <hj99.life.service@gmail.com>
2024-07-16 12:49:44 +0900

    Kotlin 중급

---
+++ Kotlin/app/src/main/java/com/hj99life/kotlin/part0/ch2/05_seadledclass.kt
@@ -0,0 +1,36 @@
+package com.hj99life.kotlin.part0.ch2
+
+fun main() {
+    val person = Person("수지", 24)
+    val dog = Dog("해피", 24)
+
+    println(person.toString())
+    println(dog.toString())
+    println(dog.copy(age = 3).toString())
+
+    val cat: Cat = BlueCat()
+    val result = when(cat) {
+        is BlueCat ->  "blue"
+        is RedCat -> "red"
+        is GreenCat -> "green"
+        is WhiteCat -> "white"
+    }
+
+    println(result)
+}
+class Person(
+    val name : String,
+    val age: Int,
+)
+
+data class Dog(
+    val name: String,
+    val age: Int,
+)
+
+//sealed class
+sealed class Cat
+class BlueCat : Cat()
+class RedCat : Cat()
+class GreenCat : Cat()
+class WhiteCat : Cat()
